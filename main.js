692fc0d006988fb9ae254fdc6b28ef68a1f92945
vinicius-robles <vinicius.robles@gmail.com>
2024-12-04 13:10:31 -0300

    Adiciona uma lógica que melhora o uso da estilização CSS ao utilizar teclas do teclado e funcionalidade de validação caso o usuário utilize elementos que não existem na aplicação.

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
-function tocaSom(idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
+function tocaSom(seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento != null && elemento.localName === 'audio') {
+        elemento.play();
+    } else {
+        console.log('Elemento não encontrado ou seletor inválido!');
+    }
 }
 
 const listaDeTeclas = document.querySelectorAll(".tecla");
@@ -13,4 +19,16 @@ for (contador = 0; contador < listaDeTeclas.length; contador++) {
     tecla.onclick = () => {
         tocaSom(idAudio);
     }
+
+    tecla.onkeydown = (evento) => {
+        if (evento.code === "Space" || evento.code === "Enter") {
+            tecla.classList.add("ativa");
+
+        }
+    }
+
+    tecla.onkeyup = () => {
+        tecla.classList.remove("ativa")
+    }
 }
+
